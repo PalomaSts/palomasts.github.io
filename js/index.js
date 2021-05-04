d694afd80a9838f249c77507392cba30cb554c70
PalomaSts <paloma.sts28@gmail.com>
2021-05-03 22:55:11 -0300

    quarto up

--- js/index.js
+++ js/index.js
@@ -6,9 +6,8 @@ function changeLanguage(language = 'pt') {
     switch (language) {
         case 'pt': {
             const bio = document.getElementById('bio');
-            bio.innerHTML = `Fascinado na área de tecnologia desde 2014, com fome de conhecimento, codar se transformou na minha paixão. 
-            Em ambito profissional, tenho boa desenvoltura em trabalho em equipe e resolução de problemas. 
-            Sempre estudando para ser a melhor versão do meu lado profissional. Focado na Stack Node.js, React.js e futuramente React Native.`
+            bio.innerHTML = `Estudante de Análise e Desenvolvimento de Sistemas proativa e empenhada, devido as minhas experiências profissionais e universitárias desenvolvi boa comunicação, organização, senso de urgência e bom desempenho nos trabalhos em equipe. 
+            Atualmente sou estagiária na área de desenvolvimento, atuando principalmente na manutenção do site do Porto de Santos.`
 
             const skills = document.getElementById('skillTitle')
             skills.innerHTML = `HABILIDADES`
@@ -20,9 +19,8 @@ function changeLanguage(language = 'pt') {
         }
         case 'es': {
             const bio = document.getElementById('bio');
-            bio.innerHTML = `Fascinado en el área de tecnología desde 2014, ávido de conocimiento, el codar se ha convertido en mi pasión.
-            En un entorno profesional, tengo buenas habilidades en trabajo en equipo y resolución de problemas.
-            Siempre estudiando para ser la mejor versión de mi lado profesional. Centrado en Stack Node.js, React.js y más tarde React Native.`
+            bio.innerHTML = `Estudiante proactivo y comprometido de Análisis y Desarrollo de Sistemas, debido a mi experiencia profesional y universitaria desarrollé buena comunicación, organización, sentido de urgencia y buen desempeño en el trabajo en equipo.
+            Actualmente soy pasante en el área de desarrollo, trabajando principalmente en el mantenimiento del sitio web del Puerto de Santos.`
 
             const skills = document.getElementById('skillTitle')
             skills.innerHTML = `HABILIDADES`
@@ -34,9 +32,8 @@ function changeLanguage(language = 'pt') {
         }
         case 'en': {
             const bio = document.getElementById('bio');
-            bio.innerHTML = `Fascinated in the technology area since 2014, hungry for knowledge, codar has become my passion.
-            In a professional environment, I have good skills in teamwork and problem solving.
-            Always studying to be the best version of my professional side. Focused on Stack Node.js, React.js and later React Native.`
+            bio.innerHTML = `Proactive and committed Systems Analysis and Development student, due to my professional and university experiences I developed good communication, organization, sense of urgency and good performance in teamwork.
+            I am currently an intern in the development area, working mainly in the maintenance of the Port of Santos website.`
 
             const skills = document.getElementById('skillTitle')
             skills.innerHTML = `SKILLS`
